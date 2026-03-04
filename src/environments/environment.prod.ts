d2daaf71dffe5f7fae384792a71a52900499dfe1
Ismael Lazaro <ismaellazaro0740@gmail.com>
2026-03-04 12:15:13 +0100

    FrontEnd sync with Render BackEnd

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  /** URL del backend en Render */
+  apiBaseUrl: 'https://kanban-app-cxdx.onrender.com'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  /** URL base del API. Vacío en desarrollo = usa el proxy (ng serve) */
+  apiBaseUrl: ''
+};
